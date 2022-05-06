2457f292928a9b89e9f2cb5c5f3e605f20f377a0
Samy Ali <samy.alisanchez91@gmail.com>
2022-05-06 09:18:27 +0200

    done

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const hbs = require("hbs");
 // require spotify-web-api-node package here:
 const SpotifyWebApi = require("spotify-web-api-node");
 
+// setting the spotify-api goes here:
+
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
+
 // Retrieve an access token
+
 spotifyApi
-  .clientCredentialsGrant()
+ .clientCredentialsGrant()
   .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
   .catch((error) =>
     console.log("Something went wrong when retrieving an access token", error)
@@ -24,7 +28,7 @@ app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 
-// setting the spotify-api goes here:
+
 
 // Our routes go here:
 app.get("/", (req, res) => {
@@ -34,9 +38,9 @@ app.get("/", (req, res) => {
 //artists
 
 app.get("/artist-search", (req, res) => {
-  const searchString = req.query.artist;
+  const cappuccino = req.query.artist;
   spotifyApi
-    .searchArtists(searchString)
+    .searchArtists(cappuccino) //searchString
     .then((data) => {
       console.log("The received data from the API: ", data.body.artists.items);
       res.render("artist-search-results", { artists: data.body.artists.items });
@@ -53,9 +57,25 @@ app.get("/albums/:id", (req, res) => {
 
   spotifyApi
     .getArtistAlbums(request)
-    .then(data => {
+    .then((data) => {
       console.log(data.body);
       res.render("albums", { albums: data.body.items });
+    })
+
+    .catch((err) =>
+      console.log("The error while searching artists occurred: ", err)
+    );
+});
+
+//tracks
+app.get("/tracks/:id", (req, res) => {
+  const request = req.params["id"];
+
+  spotifyApi
+    .getAlbumTracks(request)
+    .then(data => {
+      console.log(data.body);
+      res.render("tracks", { tracks: data.body.items });
     })
 
     .catch((err) =>
